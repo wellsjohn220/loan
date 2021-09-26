8cbf0051b5f69322ae5f0350292bffa70ba50260
John Ye <windsorjohn@220@gmail.com>
2021-09-26 15:26:09 +1000

    run online demo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+var createClass = require('create-react-class');
+
+var calculatePayment = function(principal, years, rate){
+  var monthlyRate = rate/100/12;
+  var monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1/(1 + monthlyRate), years * 12)));
+  var balance = principal;
+  for (var y=0; y<years; y++){
+    var interestY = 0;
+    var principalY = 0;
+    for (var m=0; m<12; m++){
+      var interestM = balance * monthlyRate;
+      var principalM = monthlyPayment - interestM;
+      interestY = interestY + interestM;
+      principalY = principalY + principalM;
+      balance = balance - principalM;
+    }
+  }
+  return {monthlyPayment: monthlyPayment};
+};
+
+var Header = createClass({
+  render: function () {
+    return ( 
+      <header>
+        <h1>{this.props.title}</h1>
+      </header> 
+    );
+  }
+});
+
+var MortgageCalculator =  createClass({
+  getInitialState: function(){
+    return {
+      principal: this.props.principal, years: this.props.years, rate: this.props.rate};
+    },
+    principalChange: function(event){
+      this.setState({principal: event.target.value});
+    },
+    yearsChange: function(event){
+      this.setState({years: event.target.value});
+    },
+    rateChange: function(event){
+      this.setState({rate: event.target.value});
+    },
+    render: function(){
+      var payment = calculatePayment(this.state.principal, this.state.years, this.state.rate);
+      var monthlyPayment = payment.monthlyPayment;
+      return(
+        <div className = "content">
+          <div className = "form">
+            <div>
+              <label>Principal:</label>
+              <input type="text" value={this.state.principal} onChange={this.principalChange}/>
+            </div>
+            <div>
+              <label>Years:</label>
+              <input type="text" value={this.state.years} onChange={this.yearsChange}/>
+            </div>
+             <div>
+               <label htmlFor="rate">Rate:</label>
+               <input type="text" value={this.state.rate} onChange={this.rateChange}/>
+              </div> 
+          </div>
+          <h2>Monthly Payment: <span className="currency">{Number(monthlyPayment.toFixed(2)).toLocaleString()}</span></h2>
+        </div>
+      );
+    }
+});
+var App = createClass({
+  render: function() {
+    return (
+      <div>
+        <Header title="React Mortgage Calculator" />
+        <MortgageCalculator principal="200000" years="30" rate="5" />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  // <React.StrictMode>
+    <App />,
+  // </React.StrictMode>,
+  document.getElementById('root')
+);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +94,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
